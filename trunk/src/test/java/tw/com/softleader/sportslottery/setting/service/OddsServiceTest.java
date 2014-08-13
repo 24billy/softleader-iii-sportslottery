e75b6174ceedc12a4fa97005294fa75c48faddd8
ebikyatto@gmail.com <ebikyatto@gmail.com@4226255e-94c0-e8e2-9ac9-8db6aac9f686>
2014-08-13 06:38:45 +0000

    OddsServiceTest

---
+++ trunk/src/test/java/tw/com/softleader/sportslottery/setting/service/OddsServiceTest.java
@@ -0,0 +1,38 @@
+package tw.com.softleader.sportslottery.setting.service;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import tw.com.softleader.sportslottery.common.test.BaseTest;
+import tw.com.softleader.sportslottery.setting.entity.OddsEntity;
+
+public class OddsServiceTest extends BaseTest {
+	
+	@Autowired
+	private OddsService oddsService;
+	
+	@Test
+	public void service() {
+		
+		List<OddsEntity> models = null;
+		
+		models = oddsService.getByGameId(1L);
+		
+		log.debug("getByGameId models = {}", models);
+		
+		models = oddsService.getByOddType("SU_H");
+		
+		log.debug("getByOddType('SU_H') models = {}", models);
+		
+		Long gameId = models.get(0).getGameId().getId();
+		Double oddValue = models.get(0).getOddValue().doubleValue();
+		String oddType = models.get(0).getOddType();
+		
+		assertEquals(oddValue, oddsService.getOddValue(gameId, oddType));
+	}
+
+}
