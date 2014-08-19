2538b2191a3512e25e166265868e596d959dd290
liaosophia1224@gmail.com <liaosophia1224@gmail.com@4226255e-94c0-e8e2-9ac9-8db6aac9f686>
2014-08-19 12:44:02 +0000

    game data

--- trunk/src/main/resources/db/init-setting.sql
+++ trunk/src/main/resources/db/init-setting.sql
@@ -13,15 +13,24 @@ insert into team (TEAM_NAME, create_time, creator, modified_time, modifier) valu
 insert into team (TEAM_NAME, create_time, creator, modified_time, modifier) values ('奧克蘭運動家', CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system');
 insert into team (TEAM_NAME, create_time, creator, modified_time, modifier) values ('西雅圖水手', CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system');
 insert into team (TEAM_NAME, create_time, creator, modified_time, modifier) values ('德州遊騎兵', CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system');
+insert into team (TEAM_NAME, create_time, creator, modified_time, modifier) values ('亞特蘭大勇士', CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system');
+insert into team (TEAM_NAME, create_time, creator, modified_time, modifier) values ('匹茲堡海盜', CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system');
+insert into team (TEAM_NAME, create_time, creator, modified_time, modifier) values ('亞歷桑那響尾蛇', CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system');
+insert into team (TEAM_NAME, create_time, creator, modified_time, modifier) values ('華盛頓國民', CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system');
 
-insert into game (GAME_TIME, GAME_NUM, GAME_SCORE_HOME,GAME_SCORE_AWAY,LEAGUE_NAME,BALL_TYPE, create_time, creator, modified_time, modifier,TEAM_HOME,TEAM_AWAY) values (CURRENT_TIMESTAMP, 100,2,3,'MLB','Baseball', CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system', 1, 2);
-insert into game (GAME_TIME, GAME_NUM, GAME_SCORE_HOME,GAME_SCORE_AWAY,LEAGUE_NAME,BALL_TYPE, create_time, creator, modified_time, modifier,TEAM_HOME,TEAM_AWAY) values (CURRENT_TIMESTAMP, 101,10,1,'MLB','Baseball', CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system', 2, 3);
-insert into game (GAME_TIME, GAME_NUM, GAME_SCORE_HOME,GAME_SCORE_AWAY,LEAGUE_NAME,BALL_TYPE, create_time, creator, modified_time, modifier,TEAM_HOME,TEAM_AWAY) values (CURRENT_TIMESTAMP, 102,0,9,'MLB','Baseball', CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system', 1, 3);
+
+insert into game (GAME_TIME, GAME_NUM, GAME_SCORE_HOME,GAME_SCORE_AWAY,LEAGUE_NAME,BALL_TYPE, create_time, creator, modified_time, modifier,TEAM_HOME,TEAM_AWAY) values ('2014-08-08T10:50:20', 100,2,3,'MLB','Baseball', CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system', 1, 2);
+insert into game (GAME_TIME, GAME_NUM, GAME_SCORE_HOME,GAME_SCORE_AWAY,LEAGUE_NAME,BALL_TYPE, create_time, creator, modified_time, modifier,TEAM_HOME,TEAM_AWAY) values ('2014-08-08T10:50:20', 101,10,1,'MLB','Baseball', CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system', 2, 3);
+insert into game (GAME_TIME, GAME_NUM, GAME_SCORE_HOME,GAME_SCORE_AWAY,LEAGUE_NAME,BALL_TYPE, create_time, creator, modified_time, modifier,TEAM_HOME,TEAM_AWAY) values ('2014-08-08T10:50:20', 102,0,9,'MLB','Baseball', CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system', 1, 3);
 insert into game (GAME_TIME, GAME_NUM, GAME_SCORE_HOME,GAME_SCORE_AWAY,LEAGUE_NAME,BALL_TYPE, create_time, creator, modified_time, modifier,TEAM_HOME,TEAM_AWAY) values ('2014-08-08T10:50:20', 103,0,9,'MLB','Baseball', CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system', 1, 3);
 insert into game (GAME_TIME, GAME_NUM, GAME_SCORE_HOME,GAME_SCORE_AWAY,LEAGUE_NAME,BALL_TYPE, create_time, creator, modified_time, modifier,TEAM_HOME,TEAM_AWAY) values ('2014-08-09T10:50:20', 104,0,9,'MLB','Baseball', CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system', 1, 3);
 insert into game (GAME_TIME, GAME_NUM, GAME_SCORE_HOME,GAME_SCORE_AWAY,LEAGUE_NAME,BALL_TYPE, create_time, creator, modified_time, modifier,TEAM_HOME,TEAM_AWAY) values ('2014-08-10T10:50:20', 105,0,9,'MLB','Baseball', CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system', 1, 3);
 insert into game (GAME_TIME, GAME_NUM, GAME_SCORE_HOME,GAME_SCORE_AWAY,LEAGUE_NAME,BALL_TYPE, create_time, creator, modified_time, modifier,TEAM_HOME,TEAM_AWAY) values ('2014-08-11T10:50:20', 106,0,9,'MLB','Baseball', CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system', 1, 3);
 insert into game (GAME_TIME, GAME_NUM, GAME_SCORE_HOME,GAME_SCORE_AWAY,LEAGUE_NAME,BALL_TYPE, create_time, creator, modified_time, modifier,TEAM_HOME,TEAM_AWAY) values ('2014-08-12T10:50:20', 107,0,9,'MLB','Baseball', CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system', 1, 3);
+insert into game (GAME_TIME, GAME_NUM, GAME_SCORE_HOME,GAME_SCORE_AWAY,LEAGUE_NAME,BALL_TYPE, create_time, creator, modified_time, modifier,TEAM_HOME,TEAM_AWAY) values ('2014-08-19T07:05:00', 118,0,0,'MLB','Baseball', CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system', 11, 3);
+insert into game (GAME_TIME, GAME_NUM, GAME_SCORE_HOME,GAME_SCORE_AWAY,LEAGUE_NAME,BALL_TYPE, create_time, creator, modified_time, modifier,TEAM_HOME,TEAM_AWAY) values ('2014-08-19T07:05:00', 117,0,0,'MLB','Baseball', CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system', 17, 16);
+insert into game (GAME_TIME, GAME_NUM, GAME_SCORE_HOME,GAME_SCORE_AWAY,LEAGUE_NAME,BALL_TYPE, create_time, creator, modified_time, modifier,TEAM_HOME,TEAM_AWAY, IS_END) values ('2014-08-19T07:05:00', 119,0,0,'MLB','Baseball', CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system', 19, 18, true);
+
 
 insert into USERN (USER_ACCOUNT,USER_PASSWORD,USER_NAME,USER_BIRTHDAY,USER_GENDER,USER_PHONE,USER_EMAIL, create_time, creator, modified_time, modifier) values ('jackychen','a123456','王大蠻','2014-02-28','male','0910123456','wang@gmail.com',CURRENT_TIMESTAMP,'system', CURRENT_TIMESTAMP, 'system');
 
@@ -41,6 +50,34 @@ insert into ODDS (GAME_ID, ODD_TYPE, ODD_COMBINATION, ODD_VALUE, create_time, cr
 insert into ODDS (GAME_ID, ODD_TYPE, ODD_COMBINATION, ODD_VALUE, create_time, creator, modified_time, modifier) values (7, 'SU_A', 1, 1.45, CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system');
 insert into ODDS (GAME_ID, ODD_TYPE, ODD_COMBINATION, ODD_VALUE, create_time, creator, modified_time, modifier) values (8, 'SU_H', 1, 1.25, CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system');
 insert into ODDS (GAME_ID, ODD_TYPE, ODD_COMBINATION, ODD_VALUE, create_time, creator, modified_time, modifier) values (8, 'SU_A', 1, 2.15, CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system');
+insert into ODDS (GAME_ID, ODD_TYPE, ODD_COMBINATION, ODD_VALUE, create_time, creator, modified_time, modifier) values (9, 'SU_A', 0, 2.15, CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system');
+insert into ODDS (GAME_ID, ODD_TYPE, ODD_COMBINATION, ODD_VALUE, create_time, creator, modified_time, modifier) values (9, 'SU_H', 0, 1.45, CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system');
+insert into ODDS (GAME_ID, ODD_TYPE, ODD_COMBINATION, ODD_VALUE, create_time, creator, modified_time, modifier) values (9, 'ATS_A', 1.5, 1.60, CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system');
+insert into ODDS (GAME_ID, ODD_TYPE, ODD_COMBINATION, ODD_VALUE, create_time, creator, modified_time, modifier) values (9, 'ATS_H', -1.5, 1.90, CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system');
+insert into ODDS (GAME_ID, ODD_TYPE, ODD_COMBINATION, ODD_VALUE, create_time, creator, modified_time, modifier) values (9, 'SC_H', 8.5, 1.70, CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system');
+insert into ODDS (GAME_ID, ODD_TYPE, ODD_COMBINATION, ODD_VALUE, create_time, creator, modified_time, modifier) values (9, 'SC_L', 8.5, 1.80, CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system');
+insert into ODDS (GAME_ID, ODD_TYPE, ODD_COMBINATION, ODD_VALUE, create_time, creator, modified_time, modifier) values (9, 'ODD', 0, 1.50, CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system');
+insert into ODDS (GAME_ID, ODD_TYPE, ODD_COMBINATION, ODD_VALUE, create_time, creator, modified_time, modifier) values (9, 'EVEN', 0, 2.05, CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system');
+
+
+insert into ODDS (GAME_ID, ODD_TYPE, ODD_COMBINATION, ODD_VALUE, create_time, creator, modified_time, modifier) values (10, 'SU_A', 0, 2.15, CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system');
+insert into ODDS (GAME_ID, ODD_TYPE, ODD_COMBINATION, ODD_VALUE, create_time, creator, modified_time, modifier) values (10, 'SU_H', 0, 1.45, CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system');
+insert into ODDS (GAME_ID, ODD_TYPE, ODD_COMBINATION, ODD_VALUE, create_time, creator, modified_time, modifier) values (10, 'ATS_A', 1.5, 1.60, CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system');
+insert into ODDS (GAME_ID, ODD_TYPE, ODD_COMBINATION, ODD_VALUE, create_time, creator, modified_time, modifier) values (10, 'ATS_H', -1.5, 1.90, CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system');
+insert into ODDS (GAME_ID, ODD_TYPE, ODD_COMBINATION, ODD_VALUE, create_time, creator, modified_time, modifier) values (10, 'SC_H', 7.5, 1.85, CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system');
+insert into ODDS (GAME_ID, ODD_TYPE, ODD_COMBINATION, ODD_VALUE, create_time, creator, modified_time, modifier) values (10, 'SC_L', 7.5, 1.65, CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system');
+insert into ODDS (GAME_ID, ODD_TYPE, ODD_COMBINATION, ODD_VALUE, create_time, creator, modified_time, modifier) values (10, 'ODD', 0, 1.50, CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system');
+insert into ODDS (GAME_ID, ODD_TYPE, ODD_COMBINATION, ODD_VALUE, create_time, creator, modified_time, modifier) values (10, 'EVEN', 0, 2.05, CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system');
+
+insert into ODDS (GAME_ID, ODD_TYPE, ODD_COMBINATION, ODD_VALUE, create_time, creator, modified_time, modifier) values (11, 'SU_A', 0, 2.85, CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system');
+insert into ODDS (GAME_ID, ODD_TYPE, ODD_COMBINATION, ODD_VALUE, create_time, creator, modified_time, modifier) values (11, 'SU_H', 0, 1.25, CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system');
+insert into ODDS (GAME_ID, ODD_TYPE, ODD_COMBINATION, ODD_VALUE, create_time, creator, modified_time, modifier) values (11, 'ATS_A', 1.5, 1.95, CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system');
+insert into ODDS (GAME_ID, ODD_TYPE, ODD_COMBINATION, ODD_VALUE, create_time, creator, modified_time, modifier) values (11, 'ATS_H', -1.5, 1.55, CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system');
+insert into ODDS (GAME_ID, ODD_TYPE, ODD_COMBINATION, ODD_VALUE, create_time, creator, modified_time, modifier) values (11, 'SC_H', 6.5, 1.60, CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system');
+insert into ODDS (GAME_ID, ODD_TYPE, ODD_COMBINATION, ODD_VALUE, create_time, creator, modified_time, modifier) values (11, 'SC_L', 6.5, 1.90, CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system');
+insert into ODDS (GAME_ID, ODD_TYPE, ODD_COMBINATION, ODD_VALUE, create_time, creator, modified_time, modifier) values (11, 'ODD', 0, 1.50, CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system');
+insert into ODDS (GAME_ID, ODD_TYPE, ODD_COMBINATION, ODD_VALUE, create_time, creator, modified_time, modifier) values (11, 'EVEN', 0, 2.05, CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP, 'system');
+
 
 insert into lottery (USER_ID, ODDS_ID_1, ODDS_ID_2, ODDS_ID_3, ODDS_ID_4, ODDS_ID_5, ODDS_ID_6, ODDS_ID_7, ODDS_ID_8, create_time, creator, modified_time, modifier) values (1,1,2,3,3,4,null,null,null,CURRENT_TIMESTAMP,'system', CURRENT_TIMESTAMP, 'system');
 insert into lottery (USER_ID, ODDS_ID_1, ODDS_ID_2, ODDS_ID_3, ODDS_ID_4, ODDS_ID_5, ODDS_ID_6, ODDS_ID_7, ODDS_ID_8, create_time, creator, modified_time, modifier) values (1,3,1,3,4,5,null,null,null,CURRENT_TIMESTAMP,'system', CURRENT_TIMESTAMP, 'system');
