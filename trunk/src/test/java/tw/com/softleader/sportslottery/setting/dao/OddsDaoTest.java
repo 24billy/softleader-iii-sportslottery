a2f82aad532eb86ff7bcf03d7a173485a71cc269
liaosophia1224@gmail.com <liaosophia1224@gmail.com@4226255e-94c0-e8e2-9ac9-8db6aac9f686>
2014-08-19 13:13:11 +0000

    test findForHistory

--- trunk/src/test/java/tw/com/softleader/sportslottery/setting/dao/OddsDaoTest.java
+++ trunk/src/test/java/tw/com/softleader/sportslottery/setting/dao/OddsDaoTest.java
@@ -30,60 +30,61 @@ public class OddsDaoTest extends BaseTest {
 		
 		List<OddsEntity> models = null;
 		
-		int originSize = 0;
-		int currentSize = 0;
+//		int originSize = 0;
+//		int currentSize = 0;
+//		
+//		models = oddsDao.findAll();
+//		
+//		if(models != null) {
+//			originSize = models.size();
+//		}
+//		
+//		OddsEntity entity = new OddsEntity();
+//		entity.setGameId(gameDao.findById(1L));
+//		entity.setOddType("EVEN");
+//		BigDecimal combination = new BigDecimal("1");
+//		entity.setOddCombination(combination);
+//		BigDecimal value = new BigDecimal("1");
+//		entity.setOddValue(value);
+//		
+//		oddsDao.insert(entity);
+//		
+//		models = oddsDao.findAll();
+//		
+//		if(models != null) {
+//			currentSize = models.size();
+//		}
+//		
+//		assertEquals(originSize + 1, currentSize);
+//		
+//		entity = oddsDao.findById(entity.getId());
+//		log.debug("entity = {}", entity);
+//		
+//		entity.setOddType("ODD");
+//		
+//		oddsDao.update(entity);
+//		log.debug("updated entity = {}", entity);
+//		
+//		models = oddsDao.findByOddType("ODD");
+//		log.debug("findByOddType models = {}", models);
+//		
+//		models = oddsDao.findByGameId(1L);
+//		log.debug("findByGameId models = {}", models);
+//		
+//		oddsDao.delete(entity);
+//		
+//		models = oddsDao.findAll();
+//		
+//		if(models != null) {
+//			currentSize = models.size();
+//		}
+//		
+//		assertEquals(originSize, currentSize);
+//		
 		
-		models = oddsDao.findAll();
-		
-		if(models != null) {
-			originSize = models.size();
-		}
-		
-		OddsEntity entity = new OddsEntity();
-		entity.setGameId(gameDao.findById(1L));
-		entity.setOddType("EVEN");
-		BigDecimal combination = new BigDecimal("1");
-		entity.setOddCombination(combination);
-		BigDecimal value = new BigDecimal("1");
-		entity.setOddValue(value);
-		
-		oddsDao.insert(entity);
-		
-		models = oddsDao.findAll();
-		
-		if(models != null) {
-			currentSize = models.size();
-		}
-		
-		assertEquals(originSize + 1, currentSize);
-		
-		entity = oddsDao.findById(entity.getId());
-		log.debug("entity = {}", entity);
-		
-		entity.setOddType("ODD");
-		
-		oddsDao.update(entity);
-		log.debug("updated entity = {}", entity);
-		
-		models = oddsDao.findByOddType("ODD");
-		log.debug("findByOddType models = {}", models);
-		
-		models = oddsDao.findByGameId(1L);
-		log.debug("findByGameId models = {}", models);
-		
-		oddsDao.delete(entity);
-		
-		models = oddsDao.findAll();
-		
-		if(models != null) {
-			currentSize = models.size();
-		}
-		
-		assertEquals(originSize, currentSize);
-		
-		
-		models = oddsDao.findForHistory("巴", "CURRENT_TIMESTAMP", "CURRENT_TIMESTAMP + 1");
+		models = oddsDao.findForHistory("巴", "2014-08-08T10:50:20", "2014-08-19T10:50:20");
 		log.debug("findForHistory = {}", models);
+		System.out.println("size of the searching result: "+models.size());
 		
 	}
 
