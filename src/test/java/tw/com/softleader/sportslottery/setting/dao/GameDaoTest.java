0860b3229512e465db90cd586835a7ed36c26592
liaosophia1224 <liaosophia1224@gmail.com>
2014-08-21 08:32:11 +0000

    test, need to check

--- src/test/java/tw/com/softleader/sportslottery/setting/dao/GameDaoTest.java
+++ src/test/java/tw/com/softleader/sportslottery/setting/dao/GameDaoTest.java
@@ -18,35 +18,53 @@ public class GameDaoTest extends BaseTest{
 	@Test
 	public void test() {
 		
-		int os = 0;
-		int ts = 0;
-		
+//		int os = 0;
+//		int ts = 0;
+//		
 		List<GameEntity> models = dao.findAll();
-		if (models!=null){
-			os = models.size();
-		}
+//		if (models!=null){
+//			os = models.size();
+//		}
+//		
+//		GameEntity entity = new GameEntity();
+//		LocalDateTime time = new LocalDateTime();
+//		entity.setGameTime(time);
+//		entity.setGameNum(100l);
+//		entity.setGameScoreHome(99l);
+//		entity.setGameScoreAway(101l);
+//		entity.setLeagueName("太平洋聯盟");
+//		entity.setBallType("Baseball");
+//		
+//		dao.insert(entity);
+//
+//		models = dao.findAll();
+//		if (models!=null){
+//			ts = models.size();
+//		}
+//		
+//		log.debug("after insert totalSize = {}", ts);
+//		assertEquals(os + 1, ts);
+//		
+//		models = dao.findByBallType("Baseball");
+//		log.debug("findByBallType models = {}", models);
 		
-		GameEntity entity = new GameEntity();
-		LocalDateTime time = new LocalDateTime();
-		entity.setGameTime(time);
-		entity.setGameNum(100l);
-		entity.setGameScoreHome(99l);
-		entity.setGameScoreAway(101l);
-		entity.setLeagueName("太平洋聯盟");
-		entity.setBallType("Baseball");
+		LocalDateTime date1 = new LocalDateTime();
+		date1.parse("2014-08-08T10:15:30");//將時間自串轉成 LocalDateTime
 		
-		dao.insert(entity);
-
-		models = dao.findAll();
-		if (models!=null){
-			ts = models.size();
-		}
+		LocalDateTime date2 = new LocalDateTime();
+		date2.parse("2014-08-19T07:04:00");
+		LocalDateTime date3 = new LocalDateTime();
+		date3.parse("2014-08-20T10:15:30");
 		
-		log.debug("after insert totalSize = {}", ts);
-		assertEquals(os + 1, ts);
 		
-		models = dao.findByBallType("Baseball");
-		log.debug("findByBallType models = {}", models);
+//		models = dao.findForHistory(date1, null, null);//結果數據不對, 得空值
+//		models = dao.findForHistory(date3, null, null);//結果數據不對， 得空值
+//		models = dao.findForHistory(null, date3, null);//結果數據正確
+//		models = dao.findForHistory(null, date2, null);//結果數據正確
+		models = dao.findForHistory(date2, null, null);//結果數據錯誤
+		
+		log.debug("findForHistory = {}", models);
+		System.out.println("size of the searching result: "+models.size());
 	}
 
 }
