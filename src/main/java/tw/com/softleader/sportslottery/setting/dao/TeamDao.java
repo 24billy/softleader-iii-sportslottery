dc884e04389af3ff87fce8ec5c57e900cef16f2c
liaosophia1224 <liaosophia1224@gmail.com>
2014-08-28 09:33:59 +0000

    a method to find teams by country

--- src/main/java/tw/com/softleader/sportslottery/setting/dao/TeamDao.java
+++ src/main/java/tw/com/softleader/sportslottery/setting/dao/TeamDao.java
@@ -1,5 +1,9 @@
 package tw.com.softleader.sportslottery.setting.dao;
 
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -16,5 +20,10 @@ import tw.com.softleader.sportslottery.setting.entity.TeamEntity;
 public class TeamDao  extends GenericDao<TeamEntity> {
 	@Autowired
 	private SessionFactory sessionFactory;
-
+	public List<TeamEntity> findByCountry(String country){
+		Session session = sessionFactory.getCurrentSession();
+		Query query= session.createQuery("from TeamEntity where country = :country ");
+		return query.setString("country", country).list();
+		
+	}
 }
