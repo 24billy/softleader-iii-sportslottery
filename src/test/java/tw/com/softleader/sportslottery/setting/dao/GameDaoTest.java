290d0100fccb66a6d46f328f55569c06a362bc01
liaosophia1224 <liaosophia1224@gmail.com>
2014-08-28 01:57:02 +0000

    test findHistory

--- src/test/java/tw/com/softleader/sportslottery/setting/dao/GameDaoTest.java
+++ src/test/java/tw/com/softleader/sportslottery/setting/dao/GameDaoTest.java
@@ -1,11 +1,14 @@
 package tw.com.softleader.sportslottery.setting.dao;
 
+import java.util.List;
+
 import org.joda.time.LocalDate;
 import org.joda.time.LocalDateTime;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import tw.com.softleader.sportslottery.common.test.BaseTest;
+import tw.com.softleader.sportslottery.setting.entity.GameEntity;
 
 public class GameDaoTest extends BaseTest{
 
@@ -44,21 +47,22 @@ public class GameDaoTest extends BaseTest{
 //		models = dao.findByBallType("Baseball");
 //		log.debug("findByBallType models = {}", models);
 		
-//		LocalDateTime date1 = new LocalDateTime();
-//		date1 = LocalDateTime.parse("2014-01-01T10:15:30");//將時間自串轉成 LocalDateTime
-//		LocalDateTime date2 = new LocalDateTime();
-//		date2 = LocalDateTime.parse("2014-12-31T07:04:00");
-//		LocalDateTime date3 = new LocalDateTime();
-//		date3 = LocalDateTime.parse("2014-08-20T10:15:30");
+		LocalDateTime date1 = new LocalDateTime();
+		date1 = LocalDateTime.parse("2014-01-01T10:15:30");//將時間自串轉成 LocalDateTime
+		LocalDateTime date2 = new LocalDateTime();
+		date2 = LocalDateTime.parse("2014-12-31T07:04:00");
+		LocalDateTime date3 = new LocalDateTime();
+		date3 = LocalDateTime.parse("2014-08-20T10:15:30");
 		
-//		models = dao.findForHistory(date1, null, null);//結果數據不對, 得空值
-//		models = dao.findForHistory(date3, null, null);//結果數據不對， 得空值
-//		models = dao.findForHistory(null, date3, null);//結果數據正確
-//		models = dao.findForHistory(null, date2, null);//結果數據正確
-//		models = dao.findForHistory(date1, date2, null);//結果數據錯誤
-//		
-//		log.debug("findForHistory = {}", models);
-//		System.out.println("size of the searching result: "+models.size());
+		List<GameEntity> models = null;
+		models = dao.findForHistory(date1, null, "Lamigo桃猿");
+		models = dao.findForHistory(date3, null, null);//結果數據不對， 得空值
+		models = dao.findForHistory(null, date3, null);//結果數據正確
+		models = dao.findForHistory(null, date2, null);//結果數據正確
+		models = dao.findForHistory(date1, date2, null);//結果數據錯誤
+		
+		log.debug("findForHistory = {}", models);
+////		System.out.println("size of the searching result: "+models.size());
 //----------------------------------------------------------------------------------
 //		List<GameEntity> models = dao.findGameByLeagueName("MLB");
 //		log.debug("findGameByLeagueName = {}", models);
