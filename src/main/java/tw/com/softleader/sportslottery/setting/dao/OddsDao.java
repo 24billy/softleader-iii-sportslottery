a5cb87629a4400c43d4caf5d4952205de7629089
liaosophia1224 <liaosophia1224@gmail.com>
2014-09-02 10:30:30 +0000

    countByOddType_Time_isPass

--- src/main/java/tw/com/softleader/sportslottery/setting/dao/OddsDao.java
+++ src/main/java/tw/com/softleader/sportslottery/setting/dao/OddsDao.java
@@ -134,6 +134,52 @@ public class OddsDao extends GenericDao<OddsEntity> {
 		return count;
 		
 	}
+
+	public Long countByOddType_Time_isPass(String oddType, LocalDate timeFrom, LocalDate timeTo){
+		Session session = sessionFactory.getCurrentSession();
+		
+		//設定sql字串
+		//HQL的帶入變數為 timeFrom, timeTo, teamName
+		//String sql = "select SUM(games.odds.count)from GameEntity games where games.odds.oddType= :oddType";//查詢投注類別
+		String sql = "select games.odds from GameEntity games where 1=1";//查詢投注類別
+		String sql1 = " and games.gameTime >= :timeFrom";//搜尋大於 timeFrom的時間
+		String sql2 = " and games.gameTime < :timeTo";//搜尋小於 timeTo的時間
+		//判斷是否有timeFrom 如有則加入sql1的敘述
+		boolean hasTimeFrom = false;
+		if(timeFrom != null){
+			sql += sql1;
+			hasTimeFrom = true;
+		}
+		
+		//判斷是否有timeTo 如有則加入sql2的敘述
+		boolean hasTimeTo = false;
+		if (timeTo != null) {
+			sql += sql2;
+			hasTimeTo = true;
+		}
+		
+		Query query = session.createQuery(sql);
+		if (hasTimeFrom) {
+			query.setDate("timeFrom", timeFrom.toDate());//sql沒有支援LocalDateTime, 但有支援Date, 所以轉成Date
+		}
+		if (hasTimeTo) {
+			query.setDate("timeTo", timeTo.toDate());
+		}
+		
+		//Long count = (Long)query.uniqueResult();
+		List<OddsEntity> odds = (List<OddsEntity>)query.list();
+		System.out.println(odds);
+		
+		Long count = 0L;//初始化
+		for (OddsEntity oddsEntity : odds) {
+			if(oddsEntity.getOddType().equals(oddType) && oddsEntity.getIsPass()==true ){
+				count += oddsEntity.getCount();
+			}
+		}
+		
+		return count;
+		
+	}
 	
 	
 	
