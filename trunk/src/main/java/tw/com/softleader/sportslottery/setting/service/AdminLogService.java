783dee335a418a5156467138d25023440215e3d4
ebikyatto@gmail.com <ebikyatto@gmail.com@4226255e-94c0-e8e2-9ac9-8db6aac9f686>
2014-09-09 15:03:40 +0000

    AdminLog

---
+++ trunk/src/main/java/tw/com/softleader/sportslottery/setting/service/AdminLogService.java
@@ -0,0 +1,39 @@
+package tw.com.softleader.sportslottery.setting.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import tw.com.softleader.sportslottery.common.dao.GenericDao;
+import tw.com.softleader.sportslottery.common.service.GenericService;
+import tw.com.softleader.sportslottery.setting.dao.AdminLogDao;
+import tw.com.softleader.sportslottery.setting.entity.AdminLogEntity;
+
+@Service
+public class AdminLogService extends GenericService<AdminLogEntity> {
+
+	@Autowired
+	private AdminLogDao dao;
+	
+	@Override
+	protected GenericDao<AdminLogEntity> getDao() {
+		return dao;
+	}
+	
+	public List<AdminLogEntity> getInLastMonth() {
+		return dao.findInLastMonth();
+	}
+	
+	public List<AdminLogEntity> getInLastThreeMonths() {
+		return dao.findInLastThreeMonths();
+	}
+	
+	public List<AdminLogEntity> getInLastSixMonths() {
+		return dao.findInLastSixMonths();
+	}
+	
+	public List<AdminLogEntity> getInLastYear() {
+		return dao.findInLastYear();
+	}
+}
