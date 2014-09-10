d3ad5887ec71ebe83f7065084e676ba043c6d522
ebikyatto <ebikyatto@gmail.com>
2014-09-10 03:05:09 +0000

    Announce

---
+++ src/main/java/tw/com/softleader/sportslottery/setting/entity/AnnounceEntity.java
@@ -0,0 +1,47 @@
+package tw.com.softleader.sportslottery.setting.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Type;
+import org.joda.time.LocalDateTime;
+
+import tw.com.softleader.sportslottery.common.entity.GenericEntity;
+
+@Entity
+@Table(name = "ANNOUNCE")
+public class AnnounceEntity extends GenericEntity {
+
+	private static final long serialVersionUID = 2014L;
+	
+	@Column(name = "ANNOUNCE_CONTENT")
+	private String announceContent;
+	
+	@Column(name = "ANNOUNCE_TIME")
+	@Type(type = "org.joda.time.contrib.hibernate.PersistentLocalDateTime")
+	private LocalDateTime announceTime;
+
+	@Override
+	public String toString() {
+		return "AnnounceEntity [announceContent=" + announceContent
+				+ ", announceTime=" + announceTime + ", id=" + id + "]";
+	}
+
+	public String getAnnounceContent() {
+		return announceContent;
+	}
+
+	public void setAnnounceContent(String announceContent) {
+		this.announceContent = announceContent;
+	}
+
+	public LocalDateTime getAnnounceTime() {
+		return announceTime;
+	}
+
+	public void setAnnounceTime(LocalDateTime announceTime) {
+		this.announceTime = announceTime;
+	}
+	
+}
