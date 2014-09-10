e48f2e752335a26d3dab310ea55c3747ad76a41e
liaosophia1224@gmail.com <liaosophia1224@gmail.com@4226255e-94c0-e8e2-9ac9-8db6aac9f686>
2014-09-10 09:28:19 +0000

    need to fix

---
+++ trunk/src/main/java/tw/com/softleader/sportslottery/setting/service/OddCountBean.java
@@ -0,0 +1,98 @@
+package tw.com.softleader.sportslottery.setting.service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.joda.time.LocalDate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import tw.com.softleader.sportslottery.setting.dao.GameDao;
+import tw.com.softleader.sportslottery.setting.entity.OddsEntity;
+
+public class OddCountBean {
+	private LocalDate gameTime;
+	private String teamName;
+	private Long count;
+	private Long totalCountOftheDay;//the total count of the eight types in a day;
+	private String oddType;
+	private Long countPercentage;// count/totalCountOftheDay 
+	private Boolean isPass;
+	@Autowired
+	private GameDao gameDao;
+	
+	public OddCountBean(){
+		
+	}
+	public OddCountBean(LocalDate gameTime, String teamName, Long count,
+			String oddType, Boolean isPass) {
+
+		this.gameTime = gameTime;
+		this.teamName = teamName;
+		this.count = count;
+		this.oddType = oddType;
+		this.setTotalCountOftheDay(gameTime,teamName);
+		this.setCountPercentage(count, this.totalCountOftheDay );
+		this.isPass = isPass;
+		
+		
+	}
+	public LocalDate getGameTime() {
+		return gameTime;
+	}
+	public void setGameTime(LocalDate gameTime) {
+		this.gameTime = gameTime;
+	}
+	public String getTeamName() {
+		return teamName;
+	}
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+	public Long getCount() {
+		return count;
+	}
+	public void setCount(Long count) {
+		this.count = count;
+	}
+	public Long getTotalCountOftheDay() {
+		return totalCountOftheDay;
+	}
+	public void setTotalCountOftheDay(LocalDate gameTime, String teamName) {
+		//從比賽時間和隊名瞿德投注物件list
+//		GameDao gameDao= new GameDao();
+		List<OddsEntity> OddsList= gameDao.getOddsByTimeAndTeamName(gameTime, teamName);
+		System.out.println(OddsList);
+		Long totalCountOftheDay = 0L;
+		for(OddsEntity odd: OddsList){
+			totalCountOftheDay += odd.getCount();
+			
+		}
+		
+		this.totalCountOftheDay = totalCountOftheDay;
+	}
+	public String getOddType() {
+		return oddType;
+	}
+	public void setOddType(String oddType) {
+		this.oddType = oddType;
+	}
+	public Long getCountPercentage() {
+		return countPercentage;
+	}
+	public void setCountPercentage(Long count, Long totalCountOftheDay) {
+		
+		this.countPercentage =(count/totalCountOftheDay);
+	}
+	public Boolean getIsPass() {
+		return isPass;
+	}
+	public void setIsPass(Boolean isPass) {
+		this.isPass = isPass;
+	}
+	
+
+	
+
+	
+
+}
--- trunk/src/main/java/tw/com/softleader/sportslottery/setting/service/OddsService.java
+++ trunk/src/main/java/tw/com/softleader/sportslottery/setting/service/OddsService.java
@@ -4,15 +4,18 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.joda.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 import tw.com.softleader.sportslottery.common.dao.GenericDao;
 import tw.com.softleader.sportslottery.common.service.GenericService;
+import tw.com.softleader.sportslottery.setting.dao.GameDao;
 import tw.com.softleader.sportslottery.setting.dao.LotteryDao;
 import tw.com.softleader.sportslottery.setting.dao.LotteryOddsDao;
 import tw.com.softleader.sportslottery.setting.dao.OddsDao;
+
 import tw.com.softleader.sportslottery.setting.entity.OddsEntity;
 
 import com.google.gson.Gson;
@@ -121,46 +124,78 @@ public class OddsService extends GenericService<OddsEntity> {
 	}
 	
 	//將OddsList 變成 Map
-	public Map<String,OddsEntity> from_OddsEntityList_To_OddsEntityMap(List<OddsEntity> OddsList){
-		Map<String,OddsEntity> map=new HashMap<String,OddsEntity>();
+	public Map<String, OddCountBean> from_OddsEntityList_To_OddsEntityMap(String teamName , LocalDate gameTime){
+		Map<String,OddCountBean> map=new HashMap<String,OddCountBean>();
+
+//		Long totalCount=getTotalCountOftheDay(OddsList);
 		
-		for(OddsEntity odd: OddsList ){
+		
+//		for(OddsEntity odd: OddsList ){
+//			OddCountBean bean= new OddCountBean();
+//			bean.setCount(odd.getCount());
+//			bean.setTotalCountOftheDay(totalCount);
+//			bean.setCountPercentage(bean.getCount(), totalCount);
+//			bean.setGameTime(gameTime);
+//			bean.setIsPass(odd.getIsPass());
+			//bean.setTeamName();
+		
 			
-			if(odd.getOddType().equals("SU_H")){
-				map.put("SU_H", odd);
-				System.out.println("SU_H");
-				
-			}else if(odd.getOddType().equals("SU_A")){
-				map.put("SU_A", odd);
-				System.out.println("SU_A");
-				
-			}else if(odd.getOddType().equals("ATS_A")){
-				map.put("ATS_A", odd);
-				System.out.println("ATS_A");
-				
-			}else if(odd.getOddType().equals("ATS_H")){
-				map.put("ATS_H", odd);
-				System.out.println("ATS_H");
-				
-			}else if(odd.getOddType().equals("SC_H")){
-				map.put("SC_H", odd);
-				System.out.println("SC_H");
-				
-			}else if(odd.getOddType().equals("SC_L")){
-				map.put("SC_L", odd);
-				System.out.println("SC_L");
-				
-			}else if(odd.getOddType().equals("ODD")){
-				map.put("ODD", odd);
-				System.out.println("ODD");
-				
-			}else if(odd.getOddType().equals("EVEN")){
-				map.put("EVEN", odd);
-				System.out.println("EVEN");
-				
-			}
-		}
+//			
+//			if(odd.getOddType().equals("SU_H")){
+//				bean.setOddType("SU_H");
+//				map.put("SU_H", bean);
+//				
+//				
+//			}else if(odd.getOddType().equals("SU_A")){
+//				bean.setOddType("SU_A");
+//				map.put("SU_A", bean);
+//				
+//				
+//			}else if(odd.getOddType().equals("ATS_A")){
+//				bean.setOddType("ATS_A");
+//				map.put("ATS_A", bean);
+//				
+//				
+//			}else if(odd.getOddType().equals("ATS_H")){
+//				bean.setOddType("ATS_H");
+//				map.put("ATS_H", bean);
+//				
+//				
+//			}else if(odd.getOddType().equals("SC_H")){
+//				bean.setOddType("SC_H");
+//				map.put("SC_H", bean);
+//				
+//				
+//			}else if(odd.getOddType().equals("SC_L")){
+//				bean.setOddType("SC_L");
+//				map.put("SC_L", bean);
+//				
+//				
+//			}else if(odd.getOddType().equals("ODD")){
+//				bean.setOddType("ODD");
+//				map.put("ODD", bean);
+//				
+//				
+//			}else if(odd.getOddType().equals("EVEN")){
+//				bean.setOddType("EVEN");
+//				map.put("EVEN", bean);
+//			
+//				
+//			}
+//		}
 		
 		return map;
 	}
+
+	private Long getTotalCountOftheDay(List<OddsEntity> oddsList) {
+		
+		return null;
+	}
+	
+	
+
+	
+	
+	
 }
+
