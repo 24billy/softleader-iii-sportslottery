910b4bc752bab37c2f13b1fff4b6be870fa37635
liaosophia1224@gmail.com <liaosophia1224@gmail.com@4226255e-94c0-e8e2-9ac9-8db6aac9f686>
2014-09-15 02:14:16 +0000

    testTotalCount()

--- trunk/src/test/java/tw/com/softleader/sportslottery/setting/service/GameServiceTest.java
+++ trunk/src/test/java/tw/com/softleader/sportslottery/setting/service/GameServiceTest.java
@@ -15,45 +15,51 @@ public class GameServiceTest extends BaseTest  {
 	@Autowired
 	private GameService gameService;
 	
-//	List<GameEntity> models=null;
-//	GameEntity model=null;
+////	List<GameEntity> models=null;
+////	GameEntity model=null;
+//	@Test
+//	public void testGetAll() {
+//
+//	}
+//
+//	@Test
+//	public void testGetById() {
+////		model=gameService.getById(1L);
+////		System.out.println(model);
+//	}
+//
+//	@Test
+//	public void testInsert() {
+//		
+//	}
+//
+//	@Test
+//	public void testUpdate() {
+//
+//	}
+//
+//	@Test
+//	public void testDelete() {
+//		List<GameEntity> models = gameService.getAll();
+//		int originSize = models.size();
+//		GameEntity entity = gameService.getById(11L);
+//		log.debug("entity = {}", entity);
+//		gameService.delete(entity);
+//		
+//		models = gameService.getAll();
+//		int currentSize = models.size();
+//		assertEquals(originSize - 1, currentSize);
+//	}
+//
+//	@Test
+//	public void testGetAllJSON() {
+//		
+//	}
+	
 	@Test
-	public void testGetAll() {
-
-	}
-
-	@Test
-	public void testGetById() {
-//		model=gameService.getById(1L);
-//		System.out.println(model);
-	}
-
-	@Test
-	public void testInsert() {
-		
-	}
-
-	@Test
-	public void testUpdate() {
-
-	}
-
-	@Test
-	public void testDelete() {
-		List<GameEntity> models = gameService.getAll();
-		int originSize = models.size();
-		GameEntity entity = gameService.getById(11L);
-		log.debug("entity = {}", entity);
-		gameService.delete(entity);
-		
-		models = gameService.getAll();
-		int currentSize = models.size();
-		assertEquals(originSize - 1, currentSize);
-	}
-
-	@Test
-	public void testGetAllJSON() {
-		
+	public void testTotalCount(){
+		Long totalCount=gameService.totalCount(2L);
+		log.debug("total count= {}", totalCount);
 	}
 
 }
