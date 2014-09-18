3f4561ad112e5b4eed7089c8063a8d25f4eb933f
liaosophia1224@gmail.com <liaosophia1224@gmail.com@4226255e-94c0-e8e2-9ac9-8db6aac9f686>
2014-09-18 07:09:27 +0000

    實作Comparator將用做CountBean 的Map, 依造count percentage 排序

---
+++ trunk/src/main/java/tw/com/softleader/sportslottery/setting/util/SortCountPercent.java
@@ -0,0 +1,18 @@
+package tw.com.softleader.sportslottery.setting.util;
+
+import java.util.Comparator;
+
+
+
+//compare count percentage 
+public class SortCountPercent implements Comparator<CountBean> {
+
+	@Override
+	public int compare(CountBean o1, CountBean o2) {
+		//compare count percentage
+		return o1.compareTo(o2);
+	}
+
+
+
+}
