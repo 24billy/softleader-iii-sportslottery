77d347723ee294688ead355dca41d84f1e12683c
liaosophia1224@gmail.com <liaosophia1224@gmail.com@4226255e-94c0-e8e2-9ac9-8db6aac9f686>
2014-09-30 06:26:34 +0000

    test getLatestFiveRecord

--- trunk/src/test/java/tw/com/softleader/sportslottery/setting/service/AnnounceServiceTest.java
+++ trunk/src/test/java/tw/com/softleader/sportslottery/setting/service/AnnounceServiceTest.java
@@ -1,5 +1,7 @@
 package tw.com.softleader.sportslottery.setting.service;
 
+import java.util.List;
+
 import org.joda.time.LocalDateTime;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +21,17 @@ public class AnnounceServiceTest extends BaseTest {
 		entity.setAnnounceContent("測試公告");
 		entity.setAnnounceTime(LocalDateTime.now());
 		log.debug("entity = {}", entity);
+		List<AnnounceEntity> announces= service.getLatestFiveRecord();
+		System.out.println("getLastestFiveRecord:");
+		for(AnnounceEntity e: announces){
+			System.out.println(e);
+		}
+		System.out.println("the size of getLatestFiveRecord : "+ announces.size());
+//		getLastestFiveRecord:
+//			AnnounceEntity [announceTitle=測試公告3, announceContent=測試公告內容3, announceTime=2014-08-14T10:00:00.000, modifiedTime=2014-08-15T10:30:00.000, id=3]
+//			AnnounceEntity [announceTitle=測試公告2, announceContent=測試公告內容2, announceTime=2014-08-13T08:20:10.000, modifiedTime=2014-08-13T08:21:45.000, id=2]
+//			AnnounceEntity [announceTitle=測試公告1, announceContent=測試公告內容1, announceTime=2014-08-10T10:50:20.000, modifiedTime=2014-08-12T07:20:50.000, id=1]
+//			the size of getLatestFiveRecord : 3
 	}
 
 }
